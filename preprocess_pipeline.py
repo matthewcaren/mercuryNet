564b625fd4437399978098994f3e35cc93b38ef9
Jonas Rajagopal <jrajagop@mit.edu>
2024-04-19 02:06:07 -0400

    Added lang

--- preprocess_pipeline.py
+++ preprocess_pipeline.py
@@ -50,14 +50,14 @@ def process_rows(dataFrame):
     output_dir = './vids'
     for row in dataFrame:
         # Download video and make directories
-        vid_id = f"{row[0]}_{str(row[1])}"
+        vid_id = f"{row[1]}_{str(row[2])}"
         vid_dir = os.path.join(output_dir, vid_id)
         if not os.path.exists(vid_dir):
             os.mkdir(vid_dir)
         vid_path = os.path.join(output_dir, vid_id, f'{vid_id}.mp4')
         wav_path = os.path.join(output_dir, vid_id, f'{vid_id}.wav')
         if not os.path.exists(vid_path):
-            command = f'yt-dlp -q -f "(bestvideo+bestaudio/best)[protocolup=dash]" -o {vid_id}.mp4 --download-sections *{row[1]}-{row[2]} --format 18 -P {vid_dir}  "https://www.youtube.com/watch?v={vid_id}"'
+            command = f'yt-dlp -q -f "(bestvideo+bestaudio/best)[protocolup=dash]" -o {vid_id}.mp4 --download-sections *{row[2]}-{row[3]} --format 18 -P {vid_dir}  "https://www.youtube.com/watch?v={vid_id}"'
             subprocess.run(shlex.split(command))
         if not os.path.exists(vid_path):
             os.rmdir(vid_dir)
@@ -65,7 +65,7 @@ def process_rows(dataFrame):
             # Extract audio and facial features (nill for now)
             if not os.path.exists(wav_path):
                 extract_audio_from_mp4(vid_path, wav_path)
-            facial_attributes = {'race': 0, 'gender': 0, 'emotion': 0, 'age': 0}
+            facial_attributes = {'race': 0, 'gender': 0, 'emotion': 0, 'age': 0, 'lang': row[6]}
             json.dump(facial_attributes, open(f'{vid_dir}/{vid_id}_feat.json', 'w'))
 
             # Extrace frames from video
@@ -87,7 +87,7 @@ def process_rows(dataFrame):
             np.save(f'{vid_dir}/{vid_id}_pros.npy', ground_truth)
 
             # Run YOLO on all frames
-            head_center = np.array([row[3]*y, row[4]*x])
+            head_center = np.array([row[4]*y, row[5]*x])
             batches = [frames[i:i + batch_size] for i in range(0, len(frames), batch_size)]
             counter = 0
             failed_detection = False
@@ -122,7 +122,7 @@ def process_rows(dataFrame):
                 
 if __name__ == '__main__':
     start = time.time()
-    raw_data = pd.read_csv('./data/avspeech_test.csv')
+    raw_data = pd.read_csv('./data/avspeech_test_langs.csv')
     data = np.array(raw_data)[100:110]
     process_rows(data)
     print(time.time() - start)
