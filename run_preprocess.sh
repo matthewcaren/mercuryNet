76c78ec713db9c344982ad2a3d6db12e02fa8df7
Jonas Rajagopal <jrajagop@mit.edu>
2024-04-21 23:33:12 -0400

    Added total time

--- run_preprocess.sh
+++ run_preprocess.sh
@@ -1,9 +1,12 @@
 # !/bin/bash
-
+start=`date +%s`
 rows_per_iter=10
-start_point=10800
-number_iters=20
+start_point=11200
+number_iters=10
 for ((j=start_point; j < (start_point+rows_per_iter*number_iters); j=j+rows_per_iter))
 do 
     python preprocess_pipeline.py -c $rows_per_iter -s $j
 done
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
