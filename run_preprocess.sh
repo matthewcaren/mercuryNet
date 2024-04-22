b34f0e5313852205b1a1839a534b7c0c2e2b0fc6
Jonas Rajagopal <jrajagop@mit.edu>
2024-04-22 14:53:26 -0400

    Done preprocessing

--- run_preprocess.sh
+++ run_preprocess.sh
@@ -1,8 +1,8 @@
 # !/bin/bash
 start=`date +%s`
 rows_per_iter=10
-start_point=11200
-number_iters=10
+start_point=16480
+number_iters=2
 for ((j=start_point; j < (start_point+rows_per_iter*number_iters); j=j+rows_per_iter))
 do 
     python preprocess_pipeline.py -c $rows_per_iter -s $j
