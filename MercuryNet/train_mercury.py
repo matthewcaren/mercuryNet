b90a0237e6c2d65448aa5af9112a7401b4b8b1e5
Matthew Caren <matt.t.caren@gmail.com>
2024-04-23 20:46:11 -0400

    awful training attempt

---
+++ MercuryNet/train_mercury.py
@@ -0,0 +1,31 @@
+import torch
+from model.model import MercuryNet, MercuryNetLoss
+import tqdm
+import torch.nn.functional as F
+
+
+
+def train(dataloader, model, optimizer, epoch):
+    device = torch.device("cpu")
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+
+    model = MercuryNet()
+    model.train()
+    train_loss = []
+
+    batches = tqdm(enumerate(dataloader), total=len(dataloader))
+    
+    for batch_idx, (data, target) in batches:
+        data_mps = data.to(device)
+        target_mps = target.to(device)
+        optimizer.zero_grad()
+        output = model(data_mps)
+
+        loss = MercuryNetLoss()
+        loss.backward()
+        optimizer.step()
+
+    return train_loss
