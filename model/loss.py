0e9258f6a1b7e4892ddcbb7e7e3ddbd4ad028a4d
Matthew Caren <matt.t.caren@gmail.com>
2024-05-07 18:19:09 -0400

    add distribution stats to human-readable loss

--- model/loss.py
+++ model/loss.py
@@ -67,6 +67,9 @@ class HumanReadableLoss(nn.Module):
         super(HumanReadableLoss, self).__init__()
     
     def forward(self, model_output, targets):
+        targets = targets.detach()
+        model_output = model_output.detach()
+
         nan_to_num(targets, 1e-12)
         
         target_f0, target_voiced, target_amp = targets[:,:,0], targets[:,:,1], targets[:,:,2]
@@ -94,8 +97,11 @@ class HumanReadableLoss(nn.Module):
         target_f0 -= target_f0[:,first_voiced_idx].unsqueeze(1)
         output_f0 -= output_f0[:,first_voiced_idx].unsqueeze(1)
 
-        f0_loss = F.mse_loss(output_f0, target_f0)
-        voiced_loss = F.mse_loss(target_voiced, output_voiced)
-        amp_loss = hps.amp_penalty * F.mse_loss(target_amp, output_amp)
+        f0_err = (output_f0 - target_f0)**2
+        voiced_err = (target_voiced - output_voiced)**2
+        amp_err = (target_amp - output_amp)**2
+
+        all_stats = [(torch.mean(err), torch.quantile(err, 0.5), torch.quantile(err, 0.90), torch.quantile(err, 0.95), torch.max(err))
+                     for err in (f0_err, voiced_err, amp_err)]
      
-        return f0_loss, voiced_loss, amp_loss
+        return all_stats
