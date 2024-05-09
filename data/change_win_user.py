74561cc12f29a04b525da76bc88b13d419f99618
Matthew Caren <matt.t.caren@gmail.com>
2024-05-08 22:15:06 -0400

    un-stupid win user change

--- data/change_win_user.py
+++ data/change_win_user.py
@@ -7,9 +7,9 @@ new_user = 'mcaren'
 original = np.load(PATH, allow_pickle=True)
 
 new_list = []
-for e in original:
-    original_path = e[0]
-    new_path = original_path.replace(prev_user, new_user)
+for p, [s, e] in original:
+    original_path = p
+    new_path = [original_path.replace(prev_user, new_user), [s, e]]
     new_list.append(new_path)
 
 np.save(PATH, np.array(new_list), allow_pickle=True)
